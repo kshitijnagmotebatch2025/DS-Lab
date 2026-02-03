06d2cf8b71ded259c5c59b3f30e540af0063bc64
kshitijnagmotebatch2025 <kshitij.nagmote.batch2025@sitnagpur.siu.edu.in>
2026-02-03 10:03:01 +0530

    Update 1.1.c

--- 1.1.c
+++ 1.1.c
@@ -4,21 +4,24 @@ int main(void)
 {
     //Populating the array
     int arr[10];
-    for (int i = 0; i <= 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        scanf("Enter element: %i", &arr[i]);
+        printf("Enter element %d: ", i + 1);
+        scanf("%i", &arr[i]);
     }
 
     //Searching
     int x;
-    scanf("Enter element to search: %i", &x);
-    for (int p = 0; p <= 10; p++)
+    printf("Enter element to search: ");
+    scanf("%i", &x);
+    for (int p = 0; p < 10; p++)
     {
         if (x == arr[p])
         {
-            printf("Element found at position %i", p);
-            break;
+            printf("Element found at position %i\n", p);
+            return 0;
         }
-        printf("Element not found");
     }
+    printf("Element not found\n");
+    return 0;
 }
